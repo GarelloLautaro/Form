17c8c52eb051ef3095db6db99d4f39d62c4c47d1
Lautaro Garello <lautarogarello99@gmail.com>
2020-05-24 12:57:45 -0300

    Retouching in HTML and changes in JS

--- JS/operations.js
+++ JS/operations.js
@@ -1,53 +1,49 @@
-/*var inputs = document.getElementsByClassName('frmInput')
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('keyup', function () {
-        if (this.value.length >= 1) {
-            this.nextElementSibling.classList.add('info')
-            inputs.forEach(function (inputsValuesData) {
-                console.log("El dato es :" + inputsValuesData)
-            })
-        }
-        else {
-            this.nextElementSibling.classList.remove('info')
-        }
-    })
-}*/
+username = document.getElementById('name')
+surname = document.getElementById('surname')
+email = document.getElementById('email')
+age = document.getElementById('age')
+man = document.getElementById("male")
+woman = document.getElementById("female")
+other = document.getElementById("other")
+music = document.getElementById("music")
+sports = document.getElementById("sports")
+games = document.getElementById("games")
+technology = document.getElementById("technology")
+country = document.getElementById('country').value
+argentina = document.getElementById('argentina')
+brasil = document.getElementById('brasil')
+chile = document.getElementById('chile')
+uruguay = document.getElementById('uruguay')
+comments = document.getElementById('comment')
 
-/*function viewsArrayInput() {
-    var arrayInput = new Array()
-    var inputsValues = document.getElementsByClassName('frmInput')
-    namesValues = [].map.call(inputsValues, function (datainput) {
-        arrayInput.push(datainput.value)
-    })
-    arrayInput.forEach(function (inputsValuesData) {
-        console.log("El dato es :" + inputsValuesData)
-    })
-}*/
+function sendForm() {
+    console.log('Nombre:' + username.value)
+    console.log('Apellido:' + surname.value)
+    console.log('Email:' + email.value)
+    console.log('Edad:' + age.value)
 
-const name = document.getElementById('name')
-const surname = document.getElementById('surname')
-const email = document.getElementById('email')
-const age = document.getElementById('age')
-const form = document.getElementById('frm')
-const errorElement = document.getElementById('error')
-
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Debe ingresar un nombre')
-    }
-    if (surname.value === '' || surname.value == null) {
-        messages.push('Debe ingresar un apellido')
-    }
-    if (email.value === '' || email.value == null) {
-        messages.push('Debe ingresar un email')
-    }
-    if (age.value === '' || age.value == null) {
-        messages.push('Debe ingresar una edad')
+    if (man.checked === true) {
+        console.log('Sexo: Masculino')
+    } else if (woman.checked === true) {
+        console.log('Sexo: Femenino')
+    } else if (other.checked === true) {
+        console.log('Sexo:  Otro')
     }
 
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
+    if (music.checked === true) {
+        console.log("Tema de interes: Musica")
     }
-})
+    if (sports.checked === true) {
+        console.log('Temas de interes: Deportes')
+    }
+    if (games.checked === true) {
+        console.log('Temas de interes: Juegos')
+    }
+    if (technology.checked === true) {
+        console.log('Temas de interes: Tecnologia')
+    }
+
+
+    console.log('Comentarios: ' + comments.value)
+
+}
